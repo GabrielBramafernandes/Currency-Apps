d575026b7739b8f01c8ac671bb74acc8278a9b94
Gabriel Bramafernandes <gabriel.bramafernandes@students.amikom.ac.id>
2020-10-12 14:48:12 +0700

    Add project files.

---
+++ currency apps/CurrencyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Text;
+
+namespace currency_apps
+{
+    class CurrencyController
+    {
+        public string usdToIdr(string nominal)
+        {
+            var nominalDouble = Convert.ToDouble(nominal);
+            var result = nominalDouble * 15000;
+            return Convert.ToString(result);
+        }
+
+        public Boolean isAllowed(string nominal)
+        {
+            Regex regex = new Regex("[^0-9.-]+");
+            return !regex.IsMatch(nominal);
+        }
+
+    }
+}
